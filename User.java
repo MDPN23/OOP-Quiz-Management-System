b5662c0843c981d793e331c92250dbd39482428d
DeathmarkedXD <91009001+DeathmarkedXD@users.noreply.github.com>
2023-12-30 22:16:59 +0700

    Add files via upload

---
+++ User.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pengguna;
+
+/**
+ *
+ * @author Mikail
+ */
+public abstract class User {
+    private String email;
+    private String username;
+    private String password;
+    private boolean isLoggedIn;
+
+    public User(String email, String username, String password, boolean isLoggedIn) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.isLoggedIn = isLoggedIn;
+    }
+    
+    public abstract void login(String username, String password);
+    public abstract void logout();
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setIsLoggedIn(boolean isLoggedIn) {
+        this.isLoggedIn = isLoggedIn;
+    }
+    
+    public boolean isIsLoggedIn() {
+        return isLoggedIn;
+    }
+}
