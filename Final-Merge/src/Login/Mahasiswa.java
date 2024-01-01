e4e6f92c9521550162255a8ca4e36bda3f03d0e3
Muhammad Dzikri Pandu Nareswara <85385411+MDPN23@users.noreply.github.com>
2024-01-02 00:19:58 +0700

    update login

---
+++ Final-Merge/src/Login/Mahasiswa.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Login;
+
+import java.util.*;
+import Login.*;
+
+
+/**
+ *
+ * @author Mikail
+ */
+public class Mahasiswa extends User{
+    private int nim;
+    private ArrayList<Quiz> listQuiz;
+
+    public Mahasiswa(int nim, String email, String username, String password, boolean isLoggedIn) {
+        super(email, username, password, isLoggedIn);
+        this.nim = nim;
+    }
+
+    @Override
+    public void login(String username, String password) {
+        if (username.equals(getUsername()) && password.equals(getPassword())) {
+            setIsLoggedIn(true);
+        } else {
+            setIsLoggedIn(false);
+        }
+    }
+
+    @Override
+    public void logout() {
+        
+    }
+    
+    
+}
